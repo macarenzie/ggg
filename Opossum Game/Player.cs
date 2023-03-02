dbfcbe781faea4c748d126d05d556e2aa95a37c7
Jamie Zheng <jz1149@g.rit.edu>
2023-03-01 19:31:54 -0500

    updating repo on my end- jz

--- Opossum Game/Player.cs
+++ Opossum Game/Player.cs
@@ -3,9 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
 
 namespace Opossum_Game
 {
+    /// <summary>
+    /// Will represent the object the player is controlling
+    /// Interfaces: ICollide
+    /// </summary>
     internal class Player
     {
     }
